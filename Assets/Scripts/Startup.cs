dfc1813debdfb7b73d8a3fcba21fbb21187b21aa
HungDang <84671773+Dang-22@users.noreply.github.com>
2025-02-06 16:36:01 +0700

    + Find match button

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,17 +8,18 @@ public class Startup : MonoBehaviour
     public string Client;
     public string Server;
 
-    void Start()
+    private void Start()
     {
         if (System.Environment.GetCommandLineArgs().Any(arg => arg == "-port"))
         {
             Debug.Log("Starting server");
             SceneManager.LoadScene(Server);
         }
-        else
-        {
-            Debug.Log("Starting client");
-            SceneManager.LoadScene(Client);
-        }
+    }
+
+    public void StartFind()
+    {
+        Debug.Log("Starting client");
+        SceneManager.LoadScene(Client);
     }
 }
